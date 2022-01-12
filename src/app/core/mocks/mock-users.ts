02a03dc10c468c209a780823baa64365029c7c67
LuisFeLopez <felipeln80@gmail.com>
2022-01-11 23:41:11 -0500

    Se Agrega Repo

---
+++ src/app/core/mocks/mock-users.ts
@@ -0,0 +1,7 @@
+import {User} from "../models/user.model";
+
+
+export const USERS: User[] = [
+  {id: 1, name: 'Usuario', surname: 'Usuario', email: 'usuario@gmail.com', username: 'Usuario', password: '1234'},
+  {id: 2, name: 'Admin', surname: 'Admin', email: 'Admin@gmail.com', username: 'Admin', password: '1234'},
+];
